856867c108fec4073a13b68b53c3f93f10b3062c
MinexAutomation <minex.automation@gmail.com>
2020-05-02 18:50:17 -0700

    Added default CommandLineArgumentsProvider implementation.

---
+++ source/R5T.Argumentos.Default/Code/Services/Implementations/CommandLineArgumentsProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.Argumentos.Default
+{
+    /// <summary>
+    /// The default <see cref="ICommandLineArgumentsProvider"/> implementation.
+    /// </summary>
+    public class CommandLineArgumentsProvider : ICommandLineArgumentsProvider
+    {
+        public string[] GetCommandLineArguments()
+        {
+            var commandLineArguments = Environment.GetCommandLineArgs();
+            return commandLineArguments;
+        }
+    }
+}
