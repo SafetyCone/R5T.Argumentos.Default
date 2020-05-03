f4ffaeb25c163af56bc5a380bd206d02997a0aa4
MinexAutomation <minex.automation@gmail.com>
2020-05-02 18:55:02 -0700

    Added IServiceCollectionExtensions.

---
+++ source/R5T.Argumentos.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Argumentos.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="CommandLineArgumentsProvider"/> implementation of <see cref="ICommandLineArgumentsProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultCommandLineArgumentsProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<ICommandLineArgumentsProvider, CommandLineArgumentsProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="CommandLineArgumentsProvider"/> implementation of <see cref="ICommandLineArgumentsProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultCommandLineArgumentsProvider<TCommandLineArgumentsProvider>(this IServiceCollection services)
+            where TCommandLineArgumentsProvider: ICommandLineArgumentsProvider
+        {
+            services.AddDefaultCommandLineArgumentsProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="CommandLineArgumentsProvider"/> implementation of <see cref="ICommandLineArgumentsProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ICommandLineArgumentsProvider> AddDefaultCommandLineArgumentsProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ICommandLineArgumentsProvider>(() => services.AddDefaultCommandLineArgumentsProvider());
+            return serviceAction;
+        }
+    }
+}
