505edcbc8226e2d501621b6f11c01685af0ba389
davidcoats <david@getsafetycone.com>
2022-04-27 15:25:06 -0700

    Interim changes.

--- source/R5T.Argumentos.Default/Code/Services/Implementations/CommandLineArgumentsProvider.cs
+++ source/R5T.Argumentos.Default/Code/Services/Implementations/CommandLineArgumentsProvider.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Argumentos.Default
-{
+{[ServiceImplementationMarker]
     /// <summary>
     /// The default <see cref="ICommandLineArgumentsProvider"/> implementation.
     /// </summary>
-    public class CommandLineArgumentsProvider : ICommandLineArgumentsProvider
+    public class CommandLineArgumentsProvider : ICommandLineArgumentsProvider,IServiceImplementation
     {
         public string[] GetCommandLineArguments()
         {
